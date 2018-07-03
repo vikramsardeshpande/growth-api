f7063dab9b0c57b28c1334bc2e56226f8ce28eaa
vikram <vikram@psl.com>
2018-07-03 17:36:31 +0530

    fixed comments

--- automation/jenkins/Jenkinsfile.growthapicontainer.groovy
+++ automation/jenkins/Jenkinsfile.growthapicontainer.groovy
@@ -17,17 +17,17 @@ node("dockerlinuxspot")
             }
         }
 
-       # stage("Push to dev ECR east") {
-       #        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'jenkins-aws-dev', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]){
-        #        sh "eval `aws ecr get-login --no-include-email --region us-east-1`"
-         #       sh "docker tag ${dockerTag} 239641032376.dkr.ecr.us-east-1.amazonaws.com/devops/${dockerTag}"
-          #      sh "docker push 239641032376.dkr.ecr.us-east-1.amazonaws.com/devops/${dockerTag}"
-           # }
-       # }
+       // stage("Push to dev ECR east") {
+       //       withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'jenkins-aws-dev', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]){
+        //        sh "eval `aws ecr get-login --no-include-email --region us-east-1`"
+         //       sh "docker tag ${dockerTag} 239641032376.dkr.ecr.us-east-1.amazonaws.com/devops/${dockerTag}"
+                sh "docker push 239641032376.dkr.ecr.us-east-1.amazonaws.com/devops/${dockerTag}"
+           // }
+       // }
 
-        #stage("Docker Cleanup") {
-        #    sh "docker images ${dockerTag} -q | tee ./xxx"
-         #   sh 'docker rmi `cat ./xxx` --force ||exit 0'
-        #}
+        //stage("Docker Cleanup") {
+        //   sh "docker images ${dockerTag} -q | tee ./xxx"
+         //   sh 'docker rmi `cat ./xxx` --force ||exit 0'
+        //}
     }
 }
